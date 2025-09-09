a70e4057a1498889e222864748dfdf89a30883f9
Riaraujo <96155189+Riaraujo@users.noreply.github.com>
2025-09-09 19:39:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const corsOptions = {
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:5500',
-        'http://127.0.0.1:5500'
+        'http://127.0.0.1:5500',
         'https://apexinventario.com.br'
     ],
     credentials: true,
@@ -538,4 +538,5 @@ process.on('SIGTERM', async () => {
 });
 
 startServer().catch(console.error);
+
 
